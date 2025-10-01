c867926eb8d5ee134fe36adb08f851a950b9f72a
hp <hp@ENGAHMED>
2025-10-01 22:50:29 +0300

    implementation DAL layer

---
+++ ELAREF/ELAREF.DAL/Data/Contexts/ArefDbConext.cs
@@ -0,0 +1,32 @@
+﻿using ELAREF.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELAREF.DAL.Data.Contexts
+{
+    internal class ArefDbConext : DbContext
+    {
+        public ArefDbConext() :base()
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Department> Departments { get; set; }
+        public Assembly Assemply { get; private set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=ElAref;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+
+   
+}
