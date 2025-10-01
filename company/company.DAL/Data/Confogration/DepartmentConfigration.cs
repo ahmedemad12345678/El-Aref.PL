7440488c3e61de638148e7053ce36a7d7cc8b574
hp <hp@ENGAHMED>
2025-10-01 23:44:39 +0300

    Implement Department DAL

---
+++ company/company.DAL/Data/Confogration/DepartmentConfigration.cs
@@ -0,0 +1,18 @@
+﻿using company.DAL.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace company.DAL.Data.Confogration
+{
+    internal class DepartmentConfigration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D=>D.Id).UseIdentityColumn(10,10);
+        }
+    }
+}
