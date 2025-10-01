85aa486e50b0d0d16474f56bc0e3213bc747b021
hp <hp@ENGAHMED>
2025-10-01 23:24:55 +0300

    implementaion Department DAL

---
+++ ElAREF_Group/ElAREF_Group.DAl/Data/Configration/DepartmentConfigration.cs
@@ -0,0 +1,19 @@
+﻿using ElAREF_Group.DAl.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElAREF_Group.DAl.Data.Configration
+{
+    internal class DepartmentConfigration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10,10);   
+        }
+    }
+}
---
+++ ElAREF_Group/ElAREF_Group.DAl/Data/contexts/GroupDbcontext.cs
@@ -0,0 +1,25 @@
+﻿using ElAREF_Group.DAl.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElAREF_Group.DAl.Data.contexts
+{
+    public class GroupDbcontext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=ElAref;Trusted_Connection=True;TrustServerCertificate=True;");
+            
+        }
+    }
+}
